83aa235e953a1a04157ec88a026f5e79f85778f2
Gogo1206 <maruihan2006@gmail.com>
2021-07-07 21:38:44 -0800

    app wireframe done

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'chats.dart';
+
 class chatScreen extends StatefulWidget {
   const chatScreen({Key? key}) : super(key: key);
 
@@ -12,7 +14,7 @@ class _chatScreenState extends State<chatScreen> {
   int _selectedIndex = 0;
   int index = 0;
   List<Widget> _widgetOptions = <Widget>[
-    ListView(),
+    chats(),
     Text('Contact'),
     Text('Work'),
     Text('Message'),
@@ -24,122 +26,137 @@ class _chatScreenState extends State<chatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              height: 35,
-              width: 35,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Icon(
-                Icons.arrow_back,
-                color: Colors.blueGrey,
-              ),
-            ),
-            Container(
-              height: 35,
-              width: 250,
-              padding: EdgeInsets.only(top: 8, left: 5),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextField(
-                decoration: InputDecoration.collapsed(
-                  hintText: 'Search',
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 35,
+                width: 35,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(100),
                 ),
-              ),
-            ),
-            Container(
-              height: 35,
-              width: 35,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: GestureDetector(
-                onTap: () {
-                  print('pressed');
-                },
                 child: Icon(
-                  Icons.add,
+                  Icons.arrow_back,
                   color: Colors.blueGrey,
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
-      body: Container(
-        decoration: BoxDecoration(color: Colors.black12),
-        child: Column(
-          children: [
-            Container(
-              color: Colors.white,
-              height: 30,
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Container(
-                      child: Text(
-                        'Direct Message',
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
+              Container(
+                height: 35,
+                width: 250,
+                padding: EdgeInsets.only(top: 8, left: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Search',
                   ),
-                  VerticalDivider(
-                    color: Colors.blueGrey,
-                    thickness: 2,
-                  ),
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(),
-                      child: Text(
-                        'Group Message',
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
+              Container(
+                height: 35,
+                width: 35,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    print('pressed');
+                  },
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(color: Colors.black12),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.white,
+                height: 30,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Text(
+                          'Direct Message',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                    VerticalDivider(
+                      color: Colors.blueGrey,
+                      thickness: 2,
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(),
+                        child: Text(
+                          'Group Message',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: _widgetOptions.elementAt(_selectedIndex),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.blueGrey,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              title: Text('Chat'),
             ),
-            Center(
-              child: _widgetOptions.elementAt(_selectedIndex),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contact_page_outlined),
+              title: Text('Contact'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.work),
+              title: Text('Work'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.doorbell),
+              title: Text('Notification'),
             ),
           ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTap,
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.blueGrey,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            title: Text('Chat'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contact_page_outlined),
-            title: Text('Contact'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.work),
-            title: Text('Work'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.doorbell),
-            title: Text('Notification'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTap,
       ),
     );
   }
